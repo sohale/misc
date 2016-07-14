62536c28c9b3404cdd80debb68258f58d94fe830
sohale <sohale@gmail.com>
2016-07-14 23:22:26 +0100

    Code improved. + Linter applied: warnings fixed

--- data_structures/tree_cpp-2016-7-14/build.sh
+++ data_structures/tree_cpp-2016-7-14/build.sh
@@ -1,2 +1,3 @@
+python ~/install/styleguide/cpplint/cpplint.py    *.cpp
 g++ --std=c++14 -pedantic -Wall t1node.cpp -o executable.exe \
 && ./executable.exe
--- data_structures/tree_cpp-2016-7-14/t1node.cpp
+++ data_structures/tree_cpp-2016-7-14/t1node.cpp
@@ -1,18 +1,22 @@
+// Copyright 2016 sohale
+
 #include <iostream>
 #include <vector>
+#include <string>
+const int VERBOSITY = 2;
 
 struct t1node {
     t1node* child[2];  // 8 byte for each
     std::string data;    // 8 bytes!
 
-    t1node(std::string data_){
+    explicit t1node(std::string data_) {
         data = data_;
         child[0] = NULL;
         child[1] = NULL;
     }
 };
 
-//template <typename T>
+// template <typename T>
 std::vector<t1node*> garbage_collector;
 
 t1node* new_node(std::string data) {
@@ -24,32 +28,39 @@ t1node* new_node(std::string data) {
 
 void gc() {
     int counter = 0;
-    for(auto it = garbage_collector.begin(); it < garbage_collector.end(); it++) {
-        std::cout << "delete: " << *it << std::endl << std::flush;
-        //delete *it;
+    for (auto it = garbage_collector.begin();
+             it < garbage_collector.end(); it++) {
+        if (VERBOSITY >= 2) {
+            std::cout << "delete: " << *it << std::endl << std::flush;
+        }
+        // delete *it;
         counter++;
     }
     std::cout << counter << " garbages collected." << std::endl << std::flush;
 }
 
 void traverse_inorder(t1node* root) {
-    for(int ci = 0; ci < 2; ci++){
-        traverse_inorder(root->child[0]);
-        if(ci == 0){
-            //visit
+    if (root == NULL)
+        return;
+    for (int ci = 0; ci < 2; ci++) {
+        traverse_inorder(root->child[ci]);
+        if (ci == 0) {
+            // visit
             std::cout << " " << root->data << " ";
         }
     }
 }
 
 int main() {
-    t1node* root = new_node("1");
-    //root.add_child( new_node("2"), 0);
+    t1node* root = new_node("A");
+    // root.add_child( new_node("2"), 0);
     std::cout << "root: " << sizeof(*root) << std::endl << std::flush;
-    //root->child[0]
-    t1node* z = new_node("2");
+    // root->child[0]
+    t1node* z = new_node("B");
     root->child[0] = z;
-    //(*root).child[0] = z;
+    // (*root).child[0] = z;
 
+    traverse_inorder(root);
+    std::cout << std::endl << std::flush;
     gc();
 }
